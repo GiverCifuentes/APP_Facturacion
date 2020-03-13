7ce0fa9b6f478d0cbdfccd3e4fedc6cbe6694a5e
Giver Cifuentes <gdcifuentes7@misena.edu.co>
2020-03-13 14:07:49 -0500

    Plantilla nueva

--- Program.cs
+++ Program.cs
@@ -23,194 +23,122 @@ namespace APP_Facturacion
                 Console.WriteLine("(1) para gestion de clientes (2) para gestion de productos (3) para facturación (4) Gestión de reportes (5) Para salir del programa");
                 respuestaInicio = int.Parse(Console.ReadLine());
 
-                while (respuestaInicio != 5)
+                if (respuestaInicio == 1)
                 {
-                    if (respuestaInicio == 1)
+                    //gestion clientes:
+
+                int rtaCliente;
+                Console.WriteLine("(1) Agregar Cliente (2) Modificar cliente (3) eliminar cliente (4) Listar Clientes.");
+                rtaCliente = int.Parse(Console.ReadLine());
+                    if (rtaCliente == 1)
                     {
-                        //gestion clientes:
-
-                        int rtaCliente;
-                        Console.WriteLine("(1) Agregar Cliente (2) Modificar cliente (3) eliminar cliente (4) Listar Clientes (5) salir del programa.");
-                        rtaCliente = int.Parse(Console.ReadLine());
-
-                        while (rtaCliente != 5)
-                        {
-                            if (rtaCliente == 1)
-                            {
-                                //agregar cliente
-                                Console.WriteLine("agregar clientes");
-                                Console.WriteLine("desea agregar más clientes?");
-                                string agregarMas = Console.ReadLine();
-
-                                if (agregarMas.Equals("no"))
-                                {
-                                    rtaCliente = 5;
-                                }
-
-                            }
-                            else if (rtaCliente == 2)
-                            {
-                                //modificar cliente
-                                Console.WriteLine("modiicar clientes");
-
-                                Console.WriteLine("agregar clientes");
-                                Console.WriteLine("desea modificar más clientes?");
-                                string agregarMas = Console.ReadLine();
-
-                                if (agregarMas.Equals("no"))
-                                {
-                                    rtaCliente = 5;
-                                }
-                            }
-                            else if (rtaCliente == 3)
-                            {
-                                //eliminar cliente
-                                Console.WriteLine("eliminar clientes");
-
-                                Console.WriteLine("agregar clientes");
-                                Console.WriteLine("desea eliminar más clientes?");
-                                string agregarMas = Console.ReadLine();
-
-                                if (agregarMas.Equals("no"))
-                                {
-                                    rtaCliente = 5;
-                                }
-                            }
-                            else if (rtaCliente == 4)
-                            {
-                                //listar clientes
-                                Console.WriteLine("listar clientes");
-
-                                Console.WriteLine("agregar clientes");
-                                Console.WriteLine("desea listar más clientes?");
-                                string agregarMas = Console.ReadLine();
-
-                                if (agregarMas.Equals("no"))
-                                {
-                                    rtaCliente = 5;
-                                }
-                            }
-
-                            Console.WriteLine("desea seguir en el modulo clientes?");
-                            string rtaSeguirModCliente = Console.ReadLine();
-
-                            if (rtaSeguirModCliente.Equals("no"))
-                            {
-                                rtaCliente = 5;
-                            }
-
-                        }
-                        Console.WriteLine("desea gestionar más clientes?");
-                        string rtaModClientes = Console.ReadLine();
-
-                        if (rtaModClientes.Equals("no"))
-                        {
-                            respuestaInicio = 5;
-                        }
-                    }
-                    else if (respuestaInicio == 2)
-                    {
-                        //gestion productos
-                        int rtaProductos;
-                        Console.WriteLine("(1) Agregar Producto (2) Eliminar Producto (3) listar Producto (4) Salir del Modulo Producto.");
-                        rtaProductos = int.Parse(Console.ReadLine());
-
-                        while (rtaProductos != 4)
-                        {
-                            if (rtaProductos == 1)
-                            {
-                                //Agregar producto
-                            }
-                            else if (rtaProductos == 2)
-                            {
-                                //Eliminar producto
-                            }
-                            else if (rtaProductos == 3)
-                            {
-                                //Listar producto
-                            }
-                            Console.WriteLine("desea gestionar más productos?");
-                            string rtaModProductos = Console.ReadLine();
-
-                            if (rtaModProductos.Equals("no"))
-                            {
-                                rtaProductos = 4;
-                            }
-
-                        }
+                        //TODO: agregar cliente
 
                     }
-                    else if (respuestaInicio == 3)
+                    else if (rtaCliente == 2)
                     {
-                        //facturar
-                        int rtaFacturar;
-                        Console.WriteLine("(1) Agregar Factura (2) listar todas las facturas (3) listar Por número de factura (4) Salir del Modulo Producto.");
-                        rtaFacturar = int.Parse(Console.ReadLine());
+                        //TODO: modificar cliente
 
-                        while (rtaFacturar != 4)
+                        //haga modificar cliente:
+
+                    }
+                    else if (rtaCliente == 3)
+                    {
+                        //TODO: eliminar cliente
+
+                    }
+                    else if (rtaCliente == 4)
+                    {
+                        //TODO: listar clientes
+                        int rtaListar;
+                        Console.WriteLine("(1) listar clientes por documento (2) listar todos los clientes.");
+                        rtaListar = int.Parse(Console.ReadLine());
+
+
+                        if (rtaListar == 1)
                         {
-                            if (rtaFacturar == 1)
-                            {
-                                //Agregar factura
-                            }
-                            else if (rtaFacturar == 2)
-                            {
-                                //listar todas las facturas
-                            }
-                            else if (rtaFacturar == 3)
-                            {
-                                //Listar por número de factura
-                            }
-                            Console.WriteLine("desea gestionar más facturas?");
-                            string rtaModFactura = Console.ReadLine();
-
-                            if (rtaModFactura.Equals("no"))
-                            {
-                                rtaFacturar = 4;
-                            }
+                            //listar por documento
                         }
-                    }
-                    else if (respuestaInicio == 4)
-                    {
-                        //reportes
-                        int rtaReportes;
-                        Console.WriteLine("(1) Clientes > 30 años (2) Producto con Stock<2 (3) Facturas>200.000$ (4) Salir del Modulo Producto.");
-                        rtaReportes = int.Parse(Console.ReadLine());
-
-                        while (rtaReportes != 4)
+                        else if (rtaListar == 2)
                         {
-                            if (rtaReportes == 1)
-                            {
-                                //Clientes > 30
-                            }
-                            else if (rtaReportes == 2)
-                            {
-                                //Producto Stock<2
-                            }
-                            else if (rtaReportes == 3)
-                            {
-                                //Facturas>200.000
-                            }
-                            Console.WriteLine("desea gestionar más facturas?");
-                            string rtaModFactura = Console.ReadLine();
-
-                            if (rtaModFactura.Equals("no"))
-                            {
-                                rtaReportes = 4;
-                            }
+                            //listar todos los clientes
                         }
-                    }
-
-                    Console.WriteLine("desea seguir trabajando modulos?");
-                    string rtaSeguirEnModulos = Console.ReadLine();
-
-                    if (rtaSeguirEnModulos.Equals("no"))
-                    {
-                        respuestaInicio = 5;
-                    }
-
+                    }                    
                 }
-                Console.WriteLine("desea seguir en el programa?");
+                else if (respuestaInicio == 2)
+                {
+                    //gestion productos
+                    int rtaProductos;
+                    Console.WriteLine("(1) Agregar Producto (2) Eliminar Producto (3) listar Producto");
+                    rtaProductos = int.Parse(Console.ReadLine());
+
+                    if (rtaProductos == 1)
+                    {
+                        //Agregar producto
+                    }
+                    else if (rtaProductos == 2)
+                    {
+                        //Eliminar producto
+                    }
+                    else if (rtaProductos == 3)
+                    {
+                        //Listar producto
+
+                        Console.WriteLine("listar por (1) Id Producto (2) listar todos los productos");
+                        int rtaProductosListar = int.Parse(Console.ReadLine());
+
+                        if (rtaProductosListar == 1)
+                        {
+                            //listar por Id
+                        }
+                        else if (rtaProductosListar == 2)
+                        {
+                            //listar todos los productos.
+                        }
+                    }
+                }
+                else if (respuestaInicio == 3)
+                {
+                    //facturar
+                    int rtaFacturar;
+                    Console.WriteLine("(1) Agregar Factura (2) listar todas las facturas (3) listar Por número de factura.");
+                    rtaFacturar = int.Parse(Console.ReadLine());
+
+                    if (rtaFacturar == 1)
+                    {
+                        //Agregar factura
+                    }
+                    else if (rtaFacturar == 2)
+                    {
+                        //listar todas las facturas
+                    }
+                    else if (rtaFacturar == 3)
+                    {
+                        //Listar por número de factura
+                    }
+                }
+                else if (respuestaInicio == 4)
+                {
+                    //reportes
+                    int rtaReportes;
+                    Console.WriteLine("(1) Clientes > 30 años (2) Producto con Stock<2 (3) Facturas>200.000$.");
+                    rtaReportes = int.Parse(Console.ReadLine());
+
+                    if (rtaReportes == 1)
+                    {
+                        //Clientes > 30
+                    }
+                    else if (rtaReportes == 2)
+                    {
+                        //Producto Stock<2
+                    }
+                    else if (rtaReportes == 3)
+                    {
+                        //Facturas>200.000
+                    }
+                }
+
+                Console.WriteLine("desea volver al menú principal?");
                 string rtaSeguir = Console.ReadLine();
 
                 if (rtaSeguir.Equals("no"))
